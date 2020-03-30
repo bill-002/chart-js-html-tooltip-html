5e248a5caeecbf860791e7334e83cef6f1c95d93
erik008 <erikbolinha5@gmail.com>
2020-03-30 01:12:07 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent implements OnInit  {
           style += '; border-color:' + colors.borderColor;
           style += '; border-width: 2px';
           const span = '<span class="chartjs-tooltip-key" style="' +
-          style +'"> <img src="led3.png"/> </span>';
+          style +'"> <img src="led2.png"/> </span>';
           innerHtml += '<tr><td>' + span + body + '</td></tr>';
         });
         innerHtml += '</tbody>';
